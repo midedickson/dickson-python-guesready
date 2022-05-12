b50c836a4e9d0fdfc707ba0df9175f5d92b4bb5e
MIde Dickson <sosanyaolumidedoubled@gmail.com>
2022-05-12 23:47:00 +0100

    adds schemas and rental detail fields

--- reservations/serializers.py
+++ reservations/serializers.py
@@ -3,8 +3,17 @@ from .models import Rental, Reservation
 from django.db.models import Q
 
 
+class RentalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rental
+        fields = '__all__'
+
+
 class ReservationSerializer(serializers.ModelSerializer):
     previous = serializers.SerializerMethodField()
+    rental_id = serializers.PrimaryKeyRelatedField(
+        queryset=Rental.objects.all())
+    rental = RentalSerializer(read_only=True)
 
     class Meta:
         model = Reservation
@@ -15,7 +24,7 @@ class ReservationSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
 
-        selected_rental = attrs['rental']
+        selected_rental = attrs['rental_id']
 
         reservation_exists = Reservation.objects.filter(
             Q(rental=selected_rental), Q(check_in=attrs['check_in']) | Q(check_out__gte=attrs['check_in'])).exists()
--- test_guestready/urls.py
+++ test_guestready/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+# from rest_framework.documentation import include_docs_urls
+
+schema_view = get_schema_view(title='Reservations API',
+                              description='An API to make reservations at listed rentals')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/reservations', include('reservations.urls')),
+    path('schema/', schema_view),
+    # path('docs/', include_docs_urls(title='Reservations API'))
 ]
